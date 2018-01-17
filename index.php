e82fcd590179cd321699ef62e3660d722233868d
Ousmanendiaye352 <ousmanendiaye352@gmail.com>
2018-01-17 11:38:19 +0000

    continue

--- index.php
+++ index.php
@@ -9,16 +9,59 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="inc/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <link rel="stylesheet" href="inc/css/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="inc/css/deco.css" style="text/css">
     </head>
     <body>
         <div class="contenu">
             <header>
-                
+                <nav>
+                    <div class="menu-icon">
+                        <i class="fa fa-bars fa-2x"></i>
+                    </div>
+                    <div class="logo">
+                        LOGO
+                    </div>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </header>
+            <div class="main">
+                <p>
+                    Git trouve implicitement que c'est un renommage, donc cela importe peu si vous renommez un fichier de cette manière ou avec la commande mv.
+                    La seule différence réelle est que mv ne fait qu'une commande à taper au lieu de trois — c'est une commande de convenance.
+                    Le point principal est que vous pouvez utiliser n'importe quel outil pour renommer un fichier, et traiter les commandes add/rm plus tard, avant de valider la modification.
+                    Git trouve implicitement que c'est un renommage, donc cela importe peu si vous renommez un fichier de cette manière ou avec la commande mv.
+                    La seule différence réelle est que mv ne fait qu'une commande à taper au lieu de trois — c'est une commande de convenance.
+                    Le point principal est que vous pouvez utiliser n'importe quel outil pour renommer un fichier, et traiter les commandes add/rm plus tard, avant de valider la modification.
+                </p>
+                <p>
+                    Git trouve implicitement que c'est un renommage, donc cela importe peu si vous renommez un fichier de cette manière ou avec la commande mv.
+                    La seule différence réelle est que mv ne fait qu'une commande à taper au lieu de trois — c'est une commande de convenance.
+                    Le point principal est que vous pouvez utiliser n'importe quel outil pour renommer un fichier, et traiter les commandes add/rm plus tard, avant de valider la modification.
+                    Git trouve implicitement que c'est un renommage, donc cela importe peu si vous renommez un fichier de cette manière ou avec la commande mv.
+                    La seule différence réelle est que mv ne fait qu'une commande à taper au lieu de trois — c'est une commande de convenance.
+                    Le point principal est que vous pouvez utiliser n'importe quel outil pour renommer un fichier, et traiter les commandes add/rm plus tard, avant de valider la modification.
+                </p>
+                <p>
+                    Git trouve implicitement que c'est un renommage, donc cela importe peu si vous renommez un fichier de cette manière ou avec la commande mv.
+                    La seule différence réelle est que mv ne fait qu'une commande à taper au lieu de trois — c'est une commande de convenance.
+                    Le point principal est que vous pouvez utiliser n'importe quel outil pour renommer un fichier, et traiter les commandes add/rm plus tard, avant de valider la modification.
+                    Git trouve implicitement que c'est un renommage, donc cela importe peu si vous renommez un fichier de cette manière ou avec la commande mv.
+                    La seule différence réelle est que mv ne fait qu'une commande à taper au lieu de trois — c'est une commande de convenance.
+                    Le point principal est que vous pouvez utiliser n'importe quel outil pour renommer un fichier, et traiter les commandes add/rm plus tard, avant de valider la modification.
+                </p>
+            </div>
         </div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="inc/js/dynamiq.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
